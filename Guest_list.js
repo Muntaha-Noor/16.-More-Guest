55b440a17ea997d0a922c2832dd3ae699f73638e
Muntaha-Noor <160227252+Muntaha-Noor@users.noreply.github.com>
2024-02-18 17:16:02 +0500

    Add files via upload

---
+++ Guest_list.js
@@ -0,0 +1,19 @@
+"use strict";
+let Guest_List = ['Muntaha Noor', 'Warda Noor', 'Musfirah Noor'];
+// for(let i=0; i<Guest_List.length; i++){
+//     console.log('Dear Miss.' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You\n\n')
+// }
+let absent_Guest = 'Warda Noor';
+let new_Guest = 'Kulsoom Noor';
+Guest_List[1] = new_Guest;
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You\n\n');
+}
+console.log(`Miss.${absent_Guest} is not coming to the party.\n`);
+console.log('Good News! We find Big Table So we are inviting 3 more guests.\n');
+Guest_List.unshift('Nazia Noor');
+Guest_List.splice(2, 0, 'Noor Jahan');
+Guest_List.push('Anus Noor');
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear ' + Guest_List[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You\n\n');
+}
